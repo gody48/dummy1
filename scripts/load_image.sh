b0bf5aa704b0af66d06393e4ba59b7f1f1f7e3db
demo <demo@gmail.com>
2022-07-21 15:31:38 +0530

    added in all

---
+++ scripts/load_image.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker pull 347663384581.dkr.ecr.ap-south-1.amazonaws.com/sample:v1
---
+++ scripts/start_container.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 80:80 --name httpd 347663384581.dkr.ecr.ap-south-1.amazonaws.com/sample:v1
